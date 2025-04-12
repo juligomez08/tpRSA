a10365aa7444dbdd094c452cd9291a2a71b83782
juligomez08 <gomez.julieta@gmail.com>
2025-04-12 19:30:34 -0300

    agregue un metodo que me hizo chat

--- tpo1.py
+++ tpo1.py
@@ -15,6 +15,28 @@ def mcd(e, phi):
         e = resto        # lo que me resto antes ahora es el nuevo divbisor
     return e          # cuando lleogo a 0 ya devuelve el mcd
 
+
+#def inversa_modular(e, phi):         ESTO ME LO HIZO CHAT DEBERIA SER LA COSA DE EUCLIDES EN VERSION BIEN HECHO EL MIO ESTA HARDCODEADO
+    # Guardamos valores originales
+    original_phi = phi
+    x0, x1 = 0, 1
+
+    while e > 1:
+        # Calculamos cociente
+        q = e // phi
+        # Actualizamos e y phi
+        e, phi = phi, e % phi
+        # Actualizamos x
+        x0, x1 = x1 - q * x0, x0
+
+    # Aseguramos que el resultado sea positivo
+    if x1 < 0:
+        x1 += original_phi
+
+    return x1
+
+
+
 #PIDO VALORES FIJOS DE P Y Q Y ME FIJO QUE SEAN PRIMOS DISTINTOS (MAS ADELANTE LO HACEMOS DE OTRA FORMA)
 p=int(input("p= "))
 while es_primo(p)==False:
@@ -47,15 +69,21 @@ while mcd(e, phi)!=1:
      e = int(input("e= "))
        
 
-#CALCULO D CON LA INVERSA MULTIPLICADORA
+# CÁLCULO DE LA INVERSA MODULAR D, TAL QUE: (d * e) ≡ 1 mod phi
 k = 1
-r = (1 +(k) * (phi))%(e)
-while r!=0:
-     k=k+1
-     r=(1+(k)*(phi))%(e)
-d=int((1+(k)*(phi))/(e))
+while (1 + k * phi) % e != 0:  # Buscamos el menor k tal que (1 + k * phi) sea divisible por e
+    k += 1
+# Una vez que encontramos ese k, calculamos d
+d = (1 + k * phi) // e  
 
-print("el valor de d es: ", d)
+print("El valor de d es:", d)
+
+#d = inversa_modular(e, phi)    LLAMA AL CODIGO DE CHAT
+#print("El valor de d es:", d)
+
+
+
+
 
 
 
